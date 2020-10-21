c647fc7292b8d6be7912fe53e1516204667bfb0f
MmedaraJ <72234561+MmedaraJ@users.noreply.github.com>
2020-10-21 13:12:16 -0400

    Create Dice.java

---
+++ Dice.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+/**
+ * This class implements a game dice
+ * @author Mmedara Josiah
+ * @version 1.0
+ **/
+public class Dice {
+
+	private int[] rollResults;
+    private Random r;
+
+    public Dice() {
+		r = new Random();
+    }
+
+    /**
+     * Rolls the dice n number of times
+	 * stores all results in the rollResults array
+	 * sorts the array in descending order
+	 * @param n is the number of times to roll the dice
+     **/
+    public int[] rollDice(int n) {
+		rollResults = new int[n];
+		for(int i: rollResults) {
+			rollResults[i] = 1 + r.nextInt((6 - 1) + 1);
+		}
+		//return rollResults in descending order because we always need the
+		// highest result first and second highest next and so on
+		Arrays.sort(rollResults);
+		//store the rollResults array as list using the asList() method
+		Collections.reverse(Arrays.asList(rollResults));
+		return rollResults;
+    }
+}
