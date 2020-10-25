634d833c5052defd55b68aded342c6d4f8770580
MmedaraJ <72234561+MmedaraJ@users.noreply.github.com>
2020-10-25 08:08:55 -0400

    Update Deck.java
    from arraylist to array

--- Deck.java
+++ Deck.java
@@ -7,44 +7,46 @@ import java.util.*;
  **/
 public class Deck{
 
-	private String infantry = "Infantry";
-	private String cavalry = "Cavalry";
-	private String artillery = "Artillery";
-	private Card selected;
-	private List<Card> deck;
+    private String infantry = "Infantry";
+    private String cavalry = "Cavalry";
+    private String artillery = "Artillery";
+    private Card selected;
+    private List<Card> deck;
 
-	/**
-	 * The deck should have 42 cards. Each country is evenly distributed
-	 * with either Artillery, Cavalry and Infantry as it's type
-	 * @param territories is an arraylist of 42 territories
-	**/
-	public Deck (ArrayList<Territory> territories){
-		deck = new ArrayList<>();
-		Collections.shuffle(territories);
-		for(int i =0; i<14; i++){
-			Card newCardOnDeck = new Card(territories.get(i), infantry);
-			deck.add(newCardOnDeck);
-			System.out.println(deck.get(i).getName() + " has been added to the deck.");
-		}
-		for(int i =14; i<28; i++){
-			Card newCardOnDeck = new Card(territories.get(i), cavalry);
-			deck.add(newCardOnDeck);
-			System.out.println(deck.get(i).getName() + " has been added to the deck.");
-		}
-		for(int i =28; i<42; i++){
-			Card newCardOnDeck = new Card(territories.get(i), artillery);
-			deck.add(newCardOnDeck);
-			System.out.println(deck.get(i).getName() + " has been added to the deck.");
-		}
-		Collections.shuffle(deck);
-	}
+    /**
+     * The deck should have 42 cards. Each country is evenly distributed
+     * with either Artillery, Cavalry and Infantry as it's type
+     * @param territories is an arraylist of 42 territories
+     **/
+    public Deck (Territory[] territories){
+        deck = new ArrayList<>();
 
-	/**
-	* Removes and returns the first card from the deck
-	**/
-	public Card selectCard(){
-		selected = deck.get(0);
-		deck.remove(0);
-		return selected;
-	}
+        List<Territory> territoriesList = Arrays.asList(territories);
+        Collections.shuffle(territoriesList);
+        for(int i =0; i<14; i++){
+            Card newCardOnDeck = new Card(territoriesList.get(i), infantry);
+            deck.add(newCardOnDeck);
+            System.out.println(deck.get(i).getName() + " has been added to the deck.");
+        }
+        for(int i =14; i<28; i++){
+            Card newCardOnDeck = new Card(territoriesList.get(i), cavalry);
+            deck.add(newCardOnDeck);
+            System.out.println(deck.get(i).getName() + " has been added to the deck.");
+        }
+        for(int i =28; i<42; i++){
+            Card newCardOnDeck = new Card(territoriesList.get(i), artillery);
+            deck.add(newCardOnDeck);
+            System.out.println(deck.get(i).getName() + " has been added to the deck.");
+        }
+        Collections.shuffle(deck);
+    }
+
+    /**
+     * Removes and returns the first card from the deck
+     **/
+    public Card selectCard(){
+        selected = deck.get(0);
+        deck.remove(0);
+        return selected;
+    }
 }
