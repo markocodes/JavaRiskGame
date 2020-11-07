e43cf9b81cde2243d99bb76e0c959ebe435f9c8a
Marko Majkic <marko.majkic@outlook.com>
2020-11-07 17:38:48 -0500

    adding initial testing class

---
+++ GameTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+public class GameTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        Game testGame = new Game();
+    }
+
+    // Example method signature
+    @org.junit.Test
+    public void startGame() {
+    }
+}
