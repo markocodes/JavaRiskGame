c8f17bcaffe78b383b9304262bcd8fba27bdac8b
MmedaraJ <72234561+MmedaraJ@users.noreply.github.com>
2020-11-08 13:52:19 -0500

    Update RiskView.java
    add player names dialog class

--- RiskView.java
+++ RiskView.java
@@ -151,3 +151,110 @@ class NumberOfPlayersDialog extends JDialog{
     }
 }
 
+/**
+ * sets up the menu that asks players for their names
+ * @author Mmedara Josiah
+ * @version 1.0.0
+ */
+class PlayerNamesDialog extends JDialog{
+    private JPanel playerNamesPanel;
+    private JButton startGameButton, backButton;
+    private String startGameButtonCommand = "startGameButton";
+    private String backButtonCommand = "backButton";
+    private JLabel label;
+    private JTextField player1Name, player2Name, player3Name,
+            player4Name, player5Name, player6Name;
+    private int numberOfPlayers;
+
+    /**
+     * Constructs the player names dialog box
+     * @param parent is the parent view
+     * @param modality allow use of previous windows
+     * @param numberOfPlayers number of players in the game
+     */
+    public PlayerNamesDialog(NumberOfPlayersDialog parent, boolean modality, int numberOfPlayers){
+        super(parent, modality);
+        this.numberOfPlayers = numberOfPlayers;
+
+        add(playerNamesPanel());
+        setTitle("Risk");
+        setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        pack();
+    }
+
+    /**
+     * returns the player names panel
+     * @return the player names panel
+     */
+    public JPanel playerNamesPanel(){
+        playerNamesPanel = new JPanel();
+        playerNamesPanel.setLayout(new FlowLayout());
+
+        label = new JLabel("Input your names");
+        playerNamesPanel.add(label);
+
+        player1Name = new JTextField("Player 1");
+        player2Name = new JTextField("Player 2");
+
+        playerNamesPanel.add(player1Name);
+        playerNamesPanel.add(player2Name);
+
+        if (numberOfPlayers > 2) {
+            player3Name = new JTextField("Player 3");
+            playerNamesPanel.add(player3Name);
+        }
+        if (numberOfPlayers > 3) {
+            player4Name = new JTextField("Player 4");
+            playerNamesPanel.add(player4Name);
+        }
+        if (numberOfPlayers > 4) {
+            player5Name = new JTextField("Player 5");
+            playerNamesPanel.add(player5Name);
+        }
+        if (numberOfPlayers > 5) {
+            player6Name = new JTextField("Player 6");
+            playerNamesPanel.add(player6Name);
+        }
+
+        backButton = new JButton("Back");
+        startGameButton = new JButton("Start Game");
+
+        playerNamesPanel.add(backButton);
+        playerNamesPanel.add(startGameButton);
+        setActionCommand();
+        return playerNamesPanel;
+    }
+
+    /**
+     * sets action commands for the buttons
+     */
+    public void setActionCommand(){
+        backButton.setActionCommand(backButtonCommand);
+        startGameButton.setActionCommand(startGameButtonCommand);
+    }
+
+    /**
+     * adds action listeners to the buttons
+     * @param e action event
+     */
+    public void addActionListeners(ActionListener e){
+        backButton.addActionListener(e);
+        startGameButton.addActionListener(e);
+    }
+
+    /**
+     * returns the names of all players
+     * @param playerNumber
+     * @return
+     */
+    public String getPlayersNames(int playerNumber){
+        if(playerNumber==1) return player1Name.getText();
+        else if(playerNumber==2) return player2Name.getText();
+        else if(playerNumber==3) return player3Name.getText();
+        else if(playerNumber==4) return player4Name.getText();
+        else if(playerNumber==5) return player5Name.getText();
+        else return player6Name.getText();
+    }
+}
